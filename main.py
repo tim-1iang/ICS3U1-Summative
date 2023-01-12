f947de1c02c9d29806227bf6f82b24e8bfabdc26
tim-1iang <122505919+tim-1iang@users.noreply.github.com>
2023-01-12 10:39:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-
+# Importing modules
 import pgzrun
 
-
+# Declaring the screen size
 WIDTH = 1280
 HEIGHT = 720
 
+# Declaration of variables and constants
+current_level = "tutorial"
+
+# Function to draw into the game
+def draw():
+    pass
 
 pgzrun.go()
