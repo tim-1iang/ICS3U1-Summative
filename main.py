1da124c7d44d9c961b3c773740d123fdfad4380d
tim-1iang <122505919+tim-1iang@users.noreply.github.com>
2023-01-12 11:40:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 # Importing modules
 import pgzrun
 
-# Declaring the screen size
+# Declaring the screen size5
 WIDTH = 1280
 HEIGHT = 720
 
 # Declaration of variables and constants
 current_level = "tutorial"
 
+
+# Entitiesm
+knight = Actor("resting/tile000")
+    
+
 # Function to draw into the game
 def draw():
-    pass
+    screen.clear()
+    knight.draw()
 
-pgzrun.go()
+    
+# Function to update the game
+def update():
+    if keyboard.left:
+        knight.x -= 5
+    
+    if keyboard.right:
+        knight.x += 5
+    
+
+# Background Music 
+
+if current_level == "tutorial":
+    music.play_once("tutorialmp")
+
+pgzrun.go() 
