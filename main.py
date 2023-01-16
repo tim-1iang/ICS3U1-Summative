ef97241b09640944abf100d4b89002690d15ab08
tim-1iang <122505919+tim-1iang@users.noreply.github.com>
2023-01-16 10:55:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# landing stun
 # left and right wall collision
 # ground collision
 # player camera
@@ -25,7 +24,7 @@ falling_time = 0
 jumped = False
 direction = "right"
 bad_landing_time = 0
-current_time = 0
+stunned_wait_time = 0
 
 MAX_MOVEMENT = 6
 MAX_GRAVITY = 9
@@ -51,67 +50,71 @@ def draw():
     screen.draw.filled_rect(wall1, (106, 117, 141))
     screen.draw.filled_rect(wall2, (106, 117, 141))
 
+
 def character_gravity():
     global falling_time
     falling_time += 1
     if falling_time > 0 and falling_time <= 10:
-        return (MAX_GRAVITY * 0.3)
+        return MAX_GRAVITY * 0.3
     elif falling_time > 10 and falling_time <= 20:
-        return (MAX_GRAVITY * 0.6)
+        return MAX_GRAVITY * 0.6
     elif falling_time > 20 and falling_time <= 30:
-        return (MAX_GRAVITY * 0.9)
+        return MAX_GRAVITY * 0.9
     elif falling_time > 30:
         return MAX_GRAVITY
     else:
-        return 0    
+        return 0
+
 
 def character_collision():
     pass
 
+
 def on_key_up(key):
     global framesL, framesR, STUNNED
-    # Idle animation detection 
-    if not(STUNNED):
-    
+    # Idle animation detection
+    if not (STUNNED):
+
         if key == keys.LEFT:
             idle_animation()
         if key == keys.RIGHT:
             idle_animation()
-    
-'''
+
+
+"""
 def jump():
     global movement_y
     pass
-'''
+"""
+
 
 def on_key_down(key):
     global direction, jumped, STUNNED
-    
+
     if not (STUNNED):
-        
+
         if key == keys.LEFT:
             direction = "left"
-            
+
         if key == keys.RIGHT:
             direction = "right"
-        
+
         if key == keys.Z:
             knight.y -= 0
-            #jumped = True
-            '''
+            # jumped = True
+            """
             jump()
-            '''
-            
+            """
+
         if key == keys.F:
             screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-        
+
         if key == keys.ESCAPE:
             exit()
 
         # Attack
         if key == keys.X:
             pass
-        
 
 
 # Animation for running right
@@ -127,6 +130,7 @@ def running_right_animation():
     elif framesR >= 12:
         framesR = 1
 
+
 # Aniamtion for running left
 def running_left_animation():
     global framesL
@@ -139,6 +143,7 @@ def running_left_animation():
         knight.image = "running/running_l3"
     elif framesL >= 12:
         framesL = 1
+
 
 def idle_animation():
     global direction
@@ -155,7 +160,7 @@ def fall_animation():
         temp = "l"
     elif direction == "right":
         temp = "r"
-        
+
     if falling_time > 0 and falling_time <= 10:
         knight.image = f"jumping/falling_{temp}1"
     elif falling_time > 10 and falling_time <= 20:
@@ -166,40 +171,39 @@ def fall_animation():
 
 # bad landing = When the character hits the ground at a greater velocity, the character wont be able to move for a short duration
 def landing_animation(bad_landing):
-    global direction, bad_landing_time, STUNNED, current_time
+    global direction, bad_landing_time, STUNNED, stunned_wait_time
     temp = ""
     if direction == "left":
         temp = "l"
     elif direction == "right":
         temp = "r"
-        
+
     bad_landing_time += 1
-        
+
     if bad_landing_time >= 1 and bad_landing_time <= 10:
         knight.image = f"jumping/landing_{temp}1"
+        if bad_landing:
+            STUNNED = True
+            stunned_wait_time = abs(time.time())
     elif bad_landing:
         if bad_landing_time > 10 and bad_landing_time <= 20:
             knight.image = f"jumping/landing_{temp}2"
-        elif bad_landing_time > 20 and bad_landing_time <= 30:
+        elif bad_landing_time > 20 and bad_landing_time < 30:
             knight.image = f"jumping/landing_{temp}3"
     else:
-        if bad_landing:
-            STUNNED = True
-            current_time = abs(time.time())
         falling_time = 0
-    
-    
+
 
 # Function to update the game
 def update():
-    global direction, MAX_MOVEMENT, falling_time, STUNNED, current_time
+    global direction, MAX_MOVEMENT, falling_time, STUNNED, stunned_wait_time
 
     if STUNNED:
-        if current_time == (abs(time.time) + 2):
+        if abs(time.time()) >= stunned_wait_time + 1:
             STUNNED = False
-        
+
     # Character Movement
-    if not(STUNNED):
+    if not (STUNNED):
         if keyboard.left:
             direction = "left"
             running_left_animation()
@@ -210,10 +214,9 @@ def update():
             running_right_animation()
             knight.x += MAX_MOVEMENT
 
-
     # Collision
     # Gravity / Ground Collision
-    if not(knight.colliderect(floor))and not(jumped):
+    if not (knight.colliderect(floor)) and not (jumped):
         knight.y += character_gravity()
         fall_animation()
     elif knight.colliderect(floor):
@@ -223,8 +226,6 @@ def update():
             landing_animation(True)
     else:
         falling_time = 0
-        
-        
 
 
 # Background Music
